276b2b00d084ef4fd855fa5fd4121d6e27cb4eba
Tomi <tomasgutierrezdev@gmail.com>
2023-09-09 11:35:41 -0300

    Initial commit

---
+++ src/test/java/com/guti/contactlist/ContactlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guti.contactlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
